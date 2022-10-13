e2c3a4122a7062476143b30ad0e67d5dfb1eab9e
Davi Santana Silva <davi200123@gmail.com>
2022-10-13 00:51:46 -0300

    logic complement

--- pages/Game.jsx
+++ pages/Game.jsx
@@ -1,28 +1,90 @@
 import React from "react";
+import { useState, useRef } from "react";
 
-const Game = ({ verifyLetter }) => {
+
+const Game = ({
+  verifyLetter,
+  pickedCategory,
+  pickedWord,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
+}) => {
+
+  const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    verifyLetter(letter);
+
+    setLetter("");
+
+    letterInputRef.current.focus()
+  };
+
   return (
-    <div class="game" className="h-full w-full  flex justify-center mt-32">
+    <div className="h-full w-full  flex justify-center mt-32">
       <div className="bg-project-yelllow flex flex-col h-full w-fit justify-center mt-20 rounded-lg md:scale-150">
-        <div className="bg-red-600 w-full flex justify-center text-5xl ">Adivinhe a palavra</div>
+        <div className="bg-red-600 w-full flex justify-center text-5xl ">
+          Adivinhe a palavra
+        </div>
+        <div className="bg-pink-600 flex justify-center">{score} Pontos</div>
         <div className="bg-green-600 flex justify-center pt-14 text-4xl">
           <div>Dica-</div>
-          <div className="font-bold ml-1">*xxx*</div>
+          <div className="font-bold ml-1">{pickedCategory}</div>
         </div>
-        <div className="bg-orange-600 flex justify-center pt-10 text-xl" >
-          <div> P A L A V R A </div>
-          <div> bs </div>
+        <div className="bg-orange-600 flex justify-center pt-10 text-xl">
+          {letters.map((letter, i) =>
+            guessedLetters.includes(letter) ? (
+              <div className="p-2">{letter}</div>
+            ) : (
+              <div className="p-2">_</div>
+            )
+          )}
         </div>
-        <div className="bg-blue-500 flex justify-center pt-10 text-xl font-semibold"> selecione uma letra de A-Z </div>
+        <div className="flex justify-center bg-purple-700">
+          {" "}
+          você ainda tem {guesses} tentativas{" "}
+        </div>
+        <div className="bg-blue-500 flex justify-center pt-10 text-xl font-semibold">
+          {" "}
+          selecione uma letra de A-Z{" "}
+        </div>
         <div className="flex justify-center">
-          <form action="submit" className="flex flex-col bg-red-700 w-1/2 pt-8 ">
-            <input type="text" name="letter" maxLength='1' required className="w-10 flex ml-20 "/>
-            <button onClick={verifyLetter} className="bg-project-nautilus mt-10 rounded-full w-1/2 ml-12  text-2xl border-2 outline-1 hover:bg-project-green">Revelar</button>
+          <form
+            action="submit"
+            className="flex flex-col bg-red-700 w-1/2 pt-8 "
+            onSubmit={handleSubmit}
+          >
+            <input
+              type="text"
+              name="letter"
+              maxLength="1"
+              required
+              className="w-10 flex ml-20 "
+              onChange={(e) => setLetter(e.target.value)}
+              value={letter}
+              ref={letterInputRef}
+            />
+            <button
+              onClick={verifyLetter}
+              className="bg-project-nautilus mt-10 rounded-full w-1/2 ml-12  text-2xl border-2 outline-1 hover:bg-project-green"
+            >
+              Revelar
+            </button>
           </form>
         </div>
         <div className="bg-green-600 flex justify-center text-xl">
           <div>Letras utilizadas - </div>
-          <div className="font-bold ml-2">*xxx*, -xxx-, -xxx*</div>
+          <div className="font-bold ml-2 flex">
+            {wrongLetters.map((letter, i) => (
+              <div key={i} className='mr-1' >{letter},</div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -17,6 +17,10 @@ function index() {
   const [pickedWord, setPickedWord] = useState("");
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
 
   const chooseWord = () => {
     const categories = Object.keys(words);
@@ -27,30 +31,52 @@ function index() {
     const word =
       words[category][Math.floor(Math.random() * words[category].length)];
 
-    return {word, category};
+    return { word, category };
   };
 
   const startGame = () => {
-    const {word, category} = chooseWord();
+    const { word, category } = chooseWord();
 
-    let wordLetters = word.split("")
+    let wordLetters = word.split("");
 
-    wordLetters = wordLetters.map((l) => l.toLowerCase())
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
 
-    console.log(word, category)
-    console.log(wordLetters)
+    console.log(word, category);
+    console.log(wordLetters);
 
     setPickedWord(word);
     setPickedCategory(category);
-    setLetters(letters)
+    setLetters(wordLetters);
 
     setGameStage(stages[1].name);
-    
   };
 
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
+
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter)
+    ) {
+      return;
+    }
+
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        letter,
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
   };
+
+  console.log(wrongLetters);
 
   const retry = () => {
     setGameStage(stages[0].name);
@@ -59,7 +85,18 @@ function index() {
   return (
     <div>
       {gameStage === "start" && <Home startGame={startGame} />}
-      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "game" && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
       {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
